021541c6b018abaa9ffa0de9d4bbba912427db58
User1012007 <rojasbadilloe@gmail.com>
2026-06-06 13:11:44 -0600

    rename

---
+++ code/dinov3/vit7b16v2/example.py
@@ -0,0 +1,33 @@
+from transformers import pipeline
+from transformers.image_utils import load_image
+
+url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/pipeline-cat-chonk.jpeg"
+image = load_image(url)
+
+feature_extractor = pipeline(
+    model="facebook/dinov3-vit7b16-pretrain-sat493m",
+    task="image-feature-extraction", 
+)
+features = feature_extractor(image)
+
+
+import torch
+from transformers import AutoImageProcessor, AutoModel
+from transformers.image_utils import load_image
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = load_image(url)
+
+pretrained_model_name = "facebook/dinov3-vit7b16-pretrain-sat493m"
+processor = AutoImageProcessor.from_pretrained(pretrained_model_name)
+model = AutoModel.from_pretrained(
+    pretrained_model_name, 
+    device_map="auto", 
+)
+
+inputs = processor(images=image, return_tensors="pt").to(model.device)
+with torch.inference_mode():
+    outputs = model(**inputs)
+
+pooled_output = outputs.pooler_output
+print("Pooled output shape:", pooled_output.shape)
