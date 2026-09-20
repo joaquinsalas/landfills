bac2913573fef6a7055ce4702444fb54ffcf0e81
User1012007 <rojasbadilloe@gmail.com>
2026-09-20 11:04:37 -0600

    decoder.py: minor

--- src/u-net_dinov3_sat/models/decoder.py
+++ src/u-net_dinov3_sat/models/decoder.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from models.DoubleConv import DoubleConv
 
 class Up(nn.Module):
@@ -10,5 +11,7 @@ class Up(nn.Module):
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
+        if x1.shape[-2:] != x2.shape[-2:]:
+            x1 = F.interpolate(x1, size=x2.shape[-2:], mode='bilinear', align_corners=False)
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
